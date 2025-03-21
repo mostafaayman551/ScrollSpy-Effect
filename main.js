daf638edbeddef02add9511664a39c27306025aa
Mostafa Ayman <mostafaayman551@gmail.com>
2025-03-21 23:25:30 +0200

    edit js file

--- main.js
+++ main.js
@@ -2,24 +2,50 @@ window.addEventListener("DOMContentLoaded", () => {
   const sections = document.querySelectorAll("article");
   const navLinks = document.querySelectorAll(".nav-link");
 
-  function handleScroll() {
-    const scrollPosition = window.scrollY + window.innerHeight / 2;
-    sections.forEach((section) => {
-      const sectionTop = section.offsetTop;
-      const sectionHeight = section.offsetHeight;
-      if (
-        scrollPosition >= sectionTop &&
-        scrollPosition < sectionTop + sectionHeight
-      ) {
-        navLinks.forEach((link) => {
-          link.classList.remove("active");
-        });
-        const activeLink = document.querySelector(
-          `.nav-link[href="#${section.id}"]`
-        );
-        if (activeLink) activeLink.classList.add("active");
-      }
-    });
-  }
-  window.addEventListener("scroll", handleScroll);
+// Scrollspy using IntersectionObserver Instance:-
+
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          navLinks.forEach((link) => {
+            link.classList.remove("active");
+            const activeLink = document.querySelector(
+              `.nav-link[href="#${entry.target.id}"]`
+            );
+            if (activeLink) activeLink.classList.add("active");
+          });
+        }
+      });
+    },
+    {
+      threshold: 0.5,
+    }
+  );
+  sections.forEach((section) => {
+    observer.observe(section);
+  });
+
+  // ScrollSpy with Scroll event:-
+
+  // function handleScroll() {
+  //   const scrollPosition = window.scrollY + window.innerHeight / 2;
+  //   sections.forEach((section) => {
+  //     const sectionTop = section.offsetTop;
+  //     const sectionHeight = section.offsetHeight;
+  //     if (
+  //       scrollPosition >= sectionTop &&
+  //       scrollPosition < sectionTop + sectionHeight
+  //     ) {
+  //       navLinks.forEach((link) => {
+  //         link.classList.remove("active");
+  //       });
+  //       const activeLink = document.querySelector(
+  //         `.nav-link[href="#${section.id}"]`
+  //       );
+  //       if (activeLink) activeLink.classList.add("active");
+  //     }
+  //   });
+  // }
+  // window.addEventListener("scroll", handleScroll);
 });
